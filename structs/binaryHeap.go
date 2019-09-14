8c9b82bd57120116efa04adc76d36b7979d42a23
Roman Ageev <roman.ageyev@devexpress.com>
2019-09-14 19:05:25 -0400

    binaryHeap finish

--- structs/binaryHeap.go
+++ structs/binaryHeap.go
@@ -2,54 +2,75 @@ package structs
 
 type BinaryHeap []int
 
-func NewBinaryHeap(capacity int) BinaryHeap {
-	if capacity <= 0 {
-		panic("Binary Heap capacity should be a positive number")
-	}
-
-	return make([]int, 0, capacity)
+func NewBinaryHeap() BinaryHeap {
+	return make([]int, 1)
 }
 
 func (heap BinaryHeap) Size() int {
-	return len(heap)
+	return len(heap) - 1
 }
 
-func (heap BinaryHeap) Parent(i int) int {
-	return (i - 1) / 2
+func (heap BinaryHeap) swim(i int) {
+	for i > 1 && heap[i/2] > heap[i] {
+		j := i / 2
+
+		heap[i], heap[j] = heap[j], heap[i]
+		i = j
+	}
 }
 
-func (heap BinaryHeap) Left(i int) int {
-	return i*2 + 1
+func (heap BinaryHeap) sink(i int) {
+	for i*2 <= heap.Size() {
+		j := i * 2
+
+		if j < heap.Size() && heap[j+1] < heap[j] {
+			j++
+		}
+
+		if heap[i] <= heap[j] {
+			break
+		}
+
+		heap[i], heap[j] = heap[j], heap[i]
+		i = j
+	}
 }
 
-func (heap BinaryHeap) Right(i int) int {
-	return i*2 + 2
-}
-
-func (heap BinaryHeap) Min() int {
-	return heap[0]
+func (heap BinaryHeap) Min() (int, bool) {
+	var min int
+	ok := heap.Size() > 0
+	if ok {
+		min = heap[1]
+	}
+	return min, ok
 }
 
 func (pHeap *BinaryHeap) Insert(val int) {
 	heap := *pHeap
 
 	if len(heap) == cap(heap) {
-		tmp := BinaryHeap(make([]int, len(heap), (cap(heap)+1)*2))
+		tmp := make([]int, len(heap), cap(heap)*2)
 		copy(tmp, heap)
 		heap = tmp
 	}
 
 	heap = append(heap, val)
-
-	for i := len(heap) - 1; i > 0; {
-		iParent := heap.Parent(i)
-		if heap[iParent] <= heap[i] {
-			break
-		}
-
-		heap[iParent], heap[i] = heap[i], heap[iParent]
-		i = iParent
-	}
+	heap.swim(heap.Size())
 
 	*pHeap = heap
+}
+
+func (pHeap *BinaryHeap) RemoveMin() (int, bool) {
+	heap := *pHeap
+
+	min, ok := heap.Min()
+	if ok {
+		heap[1] = heap[heap.Size()]
+		heap = heap[:heap.Size()]
+		heap.sink(1)
+
+		*pHeap = heap
+	}
+
+	return min, ok
 }
