cf165d6b31f45ea045284149d5d98a531ece482e
Roman Ageev <roman.ageyev@devexpress.com>
2019-09-21 18:25:49 -0400

    BinaryTree creation and traversals

--- structs/linkedList.go
+++ structs/linkedList.go
@@ -11,3 +11,19 @@ func NewLLNode(next *LLNode, data interface{}) *LLNode {
 		Next: next,
 	}
 }
+
+func NewLLFromArray(data []interface{}) *LLNode {
+	if len(data) == 0 {
+		return nil
+	}
+
+	head := NewLLNode(nil, data[0])
+
+	node := head
+	for i := 1; i < len(data); i++ {
+		node.Next = NewLLNode(nil, data[i])
+		node = node.Next
+	}
+
+	return head
+}
