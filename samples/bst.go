8db2d7e65c0a5b3cd8aab563f99a94e45db570a1
Roman Ageev <roman.ageyev@devexpress.com>
2019-09-23 21:33:55 -0400

    remove bst

--- samples/bst.go
+++ samples/bst.go
@@ -26,10 +26,56 @@ func InsertBST(root *structs.BTNode, val int) *structs.BTNode {
 	}
 
 	if data := root.Data.(int); val < data {
-		root.Right = InsertBST(root.Left, val)
+		root.Left = InsertBST(root.Left, val)
 	} else if val > data {
-		root.Left = InsertBST(root.Right, val)
+		root.Right = InsertBST(root.Right, val)
 	}
 
 	return root
+}
+
+func RemoveBST(root *structs.BTNode, val int) *structs.BTNode {
+	if root == nil {
+		return nil
+	}
+
+	data := root.Data.(int)
+
+	if val < data {
+		root.Left = RemoveBST(root.Left, val)
+		return root
+	}
+	if val > data {
+		root.Right = RemoveBST(root.Right, val)
+		return root
+	}
+
+	if root.Left != nil && root.Right != nil {
+		root.Left = ReplaceMinMaxBST(root, root.Left)
+		return root
+	}
+
+	if root.Left != nil {
+		return root.Left
+	}
+	if root.Right != nil {
+		return root.Right
+	}
+
+	return nil
+}
+
+func ReplaceMinMaxBST(root *structs.BTNode, node *structs.BTNode) *structs.BTNode {
+	if node.Right != nil {
+		node.Right = ReplaceMinMaxBST(root, node.Right)
+		return node
+	}
+	if node.Left != nil {
+		node.Left = ReplaceMinMaxBST(root, node.Left)
+		return node
+	}
+
+	root.Data = node.Data
+
+	return nil
 }
