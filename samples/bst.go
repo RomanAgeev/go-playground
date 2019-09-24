32dbca1dfe7a85ac1524176c32fdea8419288d5d
Roman Ageev <roman.ageyev@devexpress.com>
2019-09-23 20:51:38 -0400

    bst code refinement

--- samples/bst.go
+++ samples/bst.go
@@ -2,48 +2,33 @@ package samples
 
 import "github.com/RomanAgeev/playground/structs"
 
-func SearchBST(root *structs.BTNode, data int) (res *structs.BTNode) {
-	_, res = searchBST_rec(nil, root, data)
-	return
+func SearchBST(root *structs.BTNode, val int) *structs.BTNode {
+	if root == nil {
+		return nil
+	}
+
+	data := root.Data.(int)
+
+	if val < data {
+		return SearchBST(root.Left, val)
+	}
+
+	if val > data {
+		return SearchBST(root.Right, val)
+	}
+
+	return root
 }
 
-func searchBST_rec(parent *structs.BTNode, node *structs.BTNode, data int) (resP *structs.BTNode, resN *structs.BTNode) {
-	if node == nil {
-		resP, resN = parent, nil
-		return
+func InsertBST(root *structs.BTNode, val int) *structs.BTNode {
+	if root == nil {
+		return structs.NewBTNode(val)
 	}
 
-	switch nodeData := node.Data.(int); {
-
-	case data < nodeData:
-		resP, resN = searchBST_rec(node, node.Left, data)
-
-	case data > nodeData:
-		resP, resN = searchBST_rec(node, node.Right, data)
-
-	default:
-		resP, resN = parent, node
-	}
-	return
-}
-
-func InsertBST(root *structs.BTNode, data int) *structs.BTNode {
-	parent, node := searchBST_rec(nil, root, data)
-
-	if node != nil {
-		return root
-	}
-
-	newNode := structs.NewBTNode(data)
-
-	if parent == nil {
-		return newNode
-	}
-
-	if parentData := parent.Data.(int); parentData < data {
-		parent.Right = newNode
-	} else {
-		parent.Left = newNode
+	if data := root.Data.(int); val < data {
+		root.Right = InsertBST(root.Left, val)
+	} else if val > data {
+		root.Left = InsertBST(root.Right, val)
 	}
 
 	return root
